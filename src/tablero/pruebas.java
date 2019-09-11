2b728114e56da9084af6afe2a799af7bb4d22184
Jose <josetorro1999@hotmail.com>
2019-09-11 22:45:41 +0200

    No se puede hacer un JUnit test
    Cada vez que se ejecuta se inicia un tablero aleatorio de juego. Supongo que habrá que terminar todo lo de la interfaz para probarlo manualmente, todo lo contrario a lo que se hace, pero bueno.

---
+++ src/tablero/pruebas.java
@@ -0,0 +1,17 @@
+package tablero;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class pruebas {
+	
+	
+	Partida nuevaPartida = new Partida(4,4,3);
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
